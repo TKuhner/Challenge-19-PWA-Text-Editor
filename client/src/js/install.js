23c7b4ca6cbd477e85b820d2fa55447eeb5932ee
TKuhner <trevorkuhner29@gmail.com>
2023-07-19 14:54:25 -0500

    resolved issues and code is working. need to update readme and push to heroku

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,9 +1,9 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    
+    console.log('hit')
+    console.log("event" + event)
+    event.preventDefault();
     // Store the triggered events
     window.deferredPrompt = event;
 
@@ -11,26 +11,24 @@ window.addEventListener('beforeinstallprompt', (event) => {
     butInstall.classList.toggle('hidden', false);
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-      
-  const promptEvent = window.deferredPrompt;
+    const promptEvent = window.deferredPrompt;
+    // console.log(promptEvent)
+    if (!promptEvent) {
+        return;
+    }
 
-  if (!promptEvent) {
-   return;
-  }
+    // Show prompt
+    promptEvent.prompt();
 
-  // Show prompt
-  promptEvent.prompt();
-  
-  // Reset the deferred prompt variable, it can only be used once.
-  window.deferredPrompt = null;
-  
-  butInstall.classList.toggle('hidden', true);
+    // Reset the deferred prompt variable, it can only be used once.
+    window.deferredPrompt = null;
+
+    butInstall.classList.toggle('hidden', true);
 });
 
-// TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-      // Clear prompt
-  window.deferredPrompt = null;
-});
+    // Clear prompt
+    console.log('install hit')
+    window.deferredPrompt = null;
+}); 
