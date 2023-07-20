a84385e50d9c40f98b203e6ebefacb7eda083942
TKuhner <trevorkuhner29@gmail.com>
2023-07-19 19:13:30 -0500

    corrected db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,10 +16,10 @@ const initdb = async () =>
     console.log('PUT to the database');
   
     // Create a connection to the database database and version we want to use.
-    const contactDb = await openDB('jate', 1);
+    const jateDb = await openDB('jate', 1);
   
     // Create a new transaction and specify the database and data privileges.
-    const tx = contactDb.transaction('jate', 'readwrite');
+    const tx = jateDb.transaction('jate', 'readwrite');
   
     // Open up the desired object store.
     const store = tx.objectStore('jate');
@@ -37,10 +37,10 @@ const initdb = async () =>
     console.log('GET from the database');
   
     // Create a connection to the database database and version we want to use.
-    const contactDb = await openDB('jate', 1);
+    const jateDb = await openDB('jate', 1);
   
     // Create a new transaction and specify the database and data privileges.
-    const tx = contactDb.transaction('jate', 'readonly');
+    const tx = jateDb.transaction('jate', 'readonly');
   
     // Open up the desired object store.
     const store = tx.objectStore('jate');
